2be88b34cb22995c539de68e87c72093d05b7284
Lee_woo <dlguddn3214@naver.com>
2020-07-25 19:38:04 +0900

    first

--- src/App.js
+++ src/App.js
@@ -1,26 +1,12 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Test10 from "./useEffect/Func/Test10";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <Test10></Test10>
+        </div>
+    );
 }
 
 export default App;
